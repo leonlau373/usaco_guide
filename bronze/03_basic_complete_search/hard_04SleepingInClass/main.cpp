39bc00576959a9929257d8bd352632c20e05e8f4
Leon Lau <94111861+leonlau373@users.noreply.github.com>
2025-04-16 22:49:47 +0700

    Add files via upload

--- bronze/03_basic_complete_search/hard_04SleepingInClass/main.cpp
+++ bronze/03_basic_complete_search/hard_04SleepingInClass/main.cpp
@@ -15,7 +15,7 @@ void printvector(vector<int>& v)
     }
 }
 
-void inputvec(vector<int>& v, int n)
+void inputvec(vector<long long int>& v, int n)
 {
     for (int i = 0; i < n; i = i + 1)
     {
@@ -31,8 +31,88 @@ int main()
     //freopen("balancing.out", "w", stdout);
 
     //input method
+    int testcase;
+    cin >> testcase;
+    for(int t = 0; t < testcase; t = t + 1)
+    {
+        int n;
+        cin >> n;
+        vector<long long int> ai;
+        inputvec(ai, n);
 
+        //Solution
 
+        /*
+        Solution outline:
+        Find all numbers that is divisible by total, and for each of them
+        see if the operation is feasible or not by looking at the sum of first
+        few terms.
+
+        Example:
+        1 2 3 1 1 1
+
+        Total = 9
+        Divisible by : 1 , 3 , 9
+
+        If divisible by 1, it means that must be 9 groups of 1, not attainable
+
+        If divisible by 3, it means there must be 3 groups of 3
+        This can be attained by grouping, linear grouping from L to R:
+        (1 2) (3) (1 1 1)
+
+        Then from 6 to 3 --> we need 6 - 3 operations.
+        */
+        long long int total = 0;
+        for(int j = 0; j < n ; j = j + 1)
+        {
+            total = total + ai[j];
+        }
+
+        if(total == 0)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        long long int current_total = 0;
+
+        int i = 0;
+        long long int min_operation = 1000000;
+        while(i < n)
+        {
+            bool feasible = true;
+            current_total = current_total + ai[i];
+            if(current_total != 0 && total % current_total == 0)
+            {
+                long long int running_total = 0;
+                int j = i + 1;
+                while(running_total <= current_total && j < n)
+                {
+                    running_total = running_total + ai[j];
+                    if(running_total == current_total)
+                    {
+                        running_total = 0;
+                    }
+                    else if(running_total > current_total)
+                    {
+                        feasible = false;
+                    }
+                    j = j + 1;
+                }
+
+                if(feasible)
+                {
+                    min_operation = min(min_operation, n - total/current_total);
+                }
+
+            }
+
+            i = i + 1;
+        }
+
+        cout << min_operation << endl;
+
+    }
     return 0;
 }
 
